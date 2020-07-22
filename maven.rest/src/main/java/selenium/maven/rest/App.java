fcaf5c8c69692a8e16ffa98c43be21a8d7de0511
bhanu951123 <bhanu951123@gmail.com>
2020-07-22 17:41:47 +0530

    REST API Assignment SDET

---
+++ maven.rest/src/main/java/selenium/maven/rest/App.java
@@ -0,0 +1,13 @@
+package selenium.maven.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
